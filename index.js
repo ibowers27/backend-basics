c7d01c0233d098f258208687ed932bdb827008c5
Fahmida Hamid <fahmida.hamid@gmail.com>
2026-04-03 08:49:26 -0400

    code cleaned

--- index.js
+++ index.js
@@ -6,56 +6,6 @@ app.use(express.json()); // middleware, help parse json data
 
 const port = process.env.PORT || 3000; // use local port 3001 or any other port set by your environment
 
-//locally stored data
-
-const courses = [
-  {
-    id: 1,
-    name: "SWE",
-    enrollment: 20,
-  },
-  {
-    id: 2,
-    name: "FP",
-    enrollment: 10,
-  },
-  {
-    id: 3,
-    name: "Python",
-    enrollment: 30,
-  },
-];
-
-app.get("/api/courses", async (req, res) => {
-  res.send(courses);
-});
-
-app.get("/api/courses/:id", async (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Course does not exist...");
-  else res.send(course);
-});
-
-app.post("/api/courses", async (req, res) => {
-  const userInput = {
-    name: req.body.name,
-    enrollment: req.body.enrollment,
-  };
-  const { error, value } = validateCourse(userInput);
-
-  if (error) {
-    return res.status(400).send(error.message);
-  } else {
-    const newCourse = {
-      id: courses.length + 1,
-      name: req.body.name,
-      enrollment: req.body.enrollment,
-    };
-    courses.push(newCourse);
-    return res.send(newCourse); //convention
-  }
-});
-
 app.get("/", async (req, res) => {
   res.send("Backend Basics...");
 });
